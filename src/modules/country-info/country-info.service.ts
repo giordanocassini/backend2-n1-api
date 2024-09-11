90fc50dcf2f975306ec1128f4e8a2957485b9a27
Giordano Cassini <giordano.cassini@gmail.com>
2024-09-11 13:47:23 -0300

    feat: country-info service done

--- src/modules/country-info/country-info.service.ts
+++ src/modules/country-info/country-info.service.ts
@@ -11,13 +11,17 @@ export class CountryInfoService {
 
   async getCountryInfo(countryName: string): Promise<string> {
     try {
-      const response =
+      const countriesAPIResponse =
         await this.countriesAPI.getCountryInfoByName(countryName);
-      const countriesAPIResponse = response.data[0];
-      // countryAPIResponse possui a info da abreviação do pais que deve ser enviada pra requisição a API de noticias.
-      // você deve extrair essa informação e usa-la para fazer a requisição a api de noticias.
-      // por fim, basta juntar as informações retornadas das duas API's em um objeto só e retorna-las.
-      return JSON.stringify({}); // trocar o objeto vazio por um com info das duas apis.
+      const { cca2 } = countriesAPIResponse.data[0];
+      if (!cca2)
+        throw new Error('Country abreviation not found for ' + countryName);
+      const newsAPIResponse = await this.newsAPI.getNewsByCountry(cca2);
+      const countryInfoObj = {
+        countryInfo: countriesAPIResponse.data[0],
+        firstFoundNews: newsAPIResponse.data.articles[0],
+      };
+      return JSON.stringify(countryInfoObj);
     } catch (error) {
       throw new Error(error.message);
     }
